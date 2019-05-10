0fe5f0237e52908cdca9a58146681d29c056cd1d
Shi Kunwei <shikunwei.89@gmail.com>
2019-05-10 02:10:09 -0500

    Update change_route.sh

--- change_route.sh
+++ change_route.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #add local route in my local network environment which would enable SSR and accross GFW
-route add default gw 192.168.0.251
+route add default gw 192.168.1.251
 netstat -rn
 #   $ route delete default gw  192.168.0.1
