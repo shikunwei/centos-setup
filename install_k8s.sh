60a7d350738bd8e27acb05a27372e99f9df7e273
Shi Kunwei <shikunwei.89@gmail.com>
2019-07-25 17:37:12 +0800

    Update install_k8s.sh
    change to aliyum repo

--- install_k8s.sh
+++ install_k8s.sh
@@ -29,11 +29,11 @@ echo '1' > /proc/sys/net/bridge/bridge-nf-call-ip6tables
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
 enabled=1
 gpgcheck=1
 repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
 exclude=kube*
 EOF
 
