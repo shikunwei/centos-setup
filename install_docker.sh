53b1210c8ad6cf68ebffbeddaf93767287996b03
Shi Kunwei <shikunwei.89@gmail.com>
2019-07-25 22:49:40 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -5,7 +5,7 @@ set -e
 #   $ sh install_docker.sh
 
 # step 0: uninstall old version of docker
-sudo yum remove -y docker docker-ce docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+sudo yum remove -y docker docker-ce docker-ce-cli docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
 
 # Step 1: Install some necessary system tools
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
