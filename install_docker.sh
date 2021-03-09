49b8f5525790b4eae84726887e0fcf4795705771
Shi Kunwei <shikunwei.89@gmail.com>
2021-03-09 18:56:54 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -26,7 +26,7 @@ sudo yum install -y docker-ce docker-ce-cli
 sudo service docker start && systemctl enable docker
 
 # Step 6: install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
 
